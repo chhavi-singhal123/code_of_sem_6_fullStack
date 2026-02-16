f950fd859196480c80f8244eab7b0f9f923c7b8f
Chhavi Singhal <singhalchhavi134@gmail.comgit config --global user.email>
2026-02-16 09:45:56 +0530

    update

--- set.js
+++ set.js
@@ -28,3 +28,61 @@ setTimeout(()=>{
       nayapromise
       .then(() =>console.log("ye mera hua resolve"))
       .catch(()=> console.log("ye mera ni hua resolve"))
+
+      import fs from "fs";
+
+      fs.readFile("chhavi.txt", "utf-8", (err, data) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log(data)
+        }
+      })
+     
+      fs.promises.readFile("chhavi.txt","utf-8")
+      .then((data => console.log(data)))
+      .catch((err => console.log(err)))
+function wakeup() {
+    return Promise.resolve("mai soke utha abhi");
+}
+
+function brushkia() {
+    return Promise.resolve("mai brush kiya");
+}
+
+function bfkia() {
+    return Promise.resolve("mai breakfast kiya");
+}
+
+function wokrpegya() {
+    return Promise.resolve("mai work pe gaya");
+}
+
+function vapisaaya() {
+    return Promise.resolve("mai wapas aaya");
+}
+
+function dinnerkia() {
+    return Promise.resolve("mai dinner kiya");
+}
+wakeup()
+.then(() =>{console.log("me utha eek bar")
+return brushkia()
+})
+.then(()=>{
+    console.log("main utha 2 bar")
+    return bfkia()
+})
+.then(()=>{
+    console.log("main utha 3 bar")
+    return wokrpegya()
+})
+.then(()=>{
+    console.log("main uth 4 bar")
+    return vapisaaya()
+})
+.then(()=>{
+    console.log("main uth 5 bar")
+    return dinnerkia()
+})
